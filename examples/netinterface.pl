e424e06ba8c9f49a650128cdbc41c1cf1b803ca1
Jens Rehsack <sno@NetBSD.org>
2010-07-23 08:26:18 +0000

    minor corrections

--- examples/netinterface.pl
+++ examples/netinterface.pl
@@ -2,22 +2,20 @@
 # 	- Displays network interfaces and information
 #       - Alexander Breibach, 2010-01-05
 
+use strict;
+use warnings;
+
 use DBI;
-use Getopt::Long;
 use Text::TabularDisplay;
 
-my @opt_uid = ();    # setting the option blank, so the SQL statement runs for all users.
+my $dbh = DBI->connect("DBI:Sys:") or die $DBI::errstr;
+my $st  = $dbh->prepare("SELECT * FROM netint") or die $dbh->errstr;
+my $num = $st->execute() or die $st->errstr;
 
-my $dbh = DBI->connect("DBI:Sys:");
-my $st  =
-         $dbh->prepare("SELECT interface, address_family, address, netmask, broadcast, hwadress, flags, mtu, metric "
-                     . "FROM netint ");
-
-my $num = $st->execute();
-
-my $table = Text::TabularDisplay->new( qw(Interface AddressFamily Address Netmask Broadcast MacAddress Flags MTU Metric));
-   $table->add(@row)
-           while (@row = $st->fetchrow);
+my $row;
+my $table = Text::TabularDisplay->new( @{$st->{NAME_lc}} );
+   $table->add(@{$row})
+           while ($row = $st->fetchrow_arrayref);
     print $table->render . "\n";
 
 # GetOpt succeed, else failed
